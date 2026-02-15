267c0ac5f99141f7b6a39e6b5bed80cc0f63919d
Sliliia85 <liliyasimbukhova@gmail.com>
2026-02-15 08:09:56 +0100

    trasfet notesDetails in note  id

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from '../../../components/NoteDetails/NoteDetails';
+import NoteDetailsClient from './NoteDetails/NoteDetails';
 import ModalWrapper from './ModalWrapper';
 
 interface Props {
